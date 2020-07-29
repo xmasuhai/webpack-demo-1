71b33f715f3ae316cf6d12afe37ae9393dcf94c6
JoelXu <xmasuhai@163.com>
2020-07-30 06:10:28 +0800

    remove dist

--- src/dist/loadButton.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _ = _interopRequireDefault(require("./assets/1.png"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-//# sourceMappingURL=loadButton.dev.js.map
